253f3519b382521eae7c74d1878062b7dd3bfd1d
Juan Nicolás Calle <juan_nico14@hotmail.com>
2021-07-24 00:01:28 -0500

    niveles listos para editar

--- Assets/Scripts/Personaje.cs
+++ Assets/Scripts/Personaje.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using MoreMountains.Feedbacks;
 public class Personaje : MonoBehaviour
 {
     public NivelControl nivelControl;
